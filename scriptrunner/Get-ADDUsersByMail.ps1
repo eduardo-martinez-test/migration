7b7614277d47b46208fab28ae02a50863681eacd
Eduardo Martínez <eduardo.martinez@nintendo.es>
2022-09-01 10:42:00 +0200

    deleted other file

--- scriptrunner/Get-ADDUsersByMail.ps1
+++
@@ -1,7 +0,0 @@
-foreach ($user in Get-Content .\usersaad2.txt){
-    $result = Get-AzureADUser -SearchString "$user"
-    if($result){
-        Write-Output "`nUser $user added to group"
-        Add-AzureADGroupMember -ObjectId "d8547cc4-1ea0-47e8-a139-07fbb151afc3" -RefObjectId "$($result.ObjectId)"
-    }
-}
